7f47fdb67fc1a0c76d7a1f0906a399d19f7baa84
Kyle <kylepatterson91@outlook.com>
2020-02-12 15:44:15 +0000

    removed old stuff

--- LRentals/settings.py
+++ LRentals/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!-=7p_3a!ywe0w8t9vavf@ga6v)eyyg#fsv6zdw!$$c2i96ea5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['daretodream.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 LOGIN_URL = '/login'
 
